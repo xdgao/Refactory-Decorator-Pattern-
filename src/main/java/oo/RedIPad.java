79ea6dcb815ceb25647b26cda5f3c217c1bd7a3b
Xiduan Gao <gaoxiduan@gmail.com>
2013-05-21 11:27:09 +0800

    step-1 : 在子类中创建一个父类类型相同的属性

--- src/main/java/oo/RedIPad.java
+++ src/main/java/oo/RedIPad.java
@@ -1,6 +1,9 @@
 package oo;
 
 public class RedIPad extends BlackIPad {
+
+    private IPad iPad;
+
     public int getPrice() {
         return super.getPrice() + 400;
     }
--- src/main/java/oo/WhiteIPad.java
+++ src/main/java/oo/WhiteIPad.java
@@ -1,6 +1,9 @@
 package oo;
 
 public class WhiteIPad extends BlackIPad {
+
+    private IPad iPad;
+
     public int getPrice() {
         return super.getPrice() + 200;
     }
